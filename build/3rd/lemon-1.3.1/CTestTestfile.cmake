7a6682ff2042461885a8ca8a64b8cb26c4034e9d
colbyj427 <52146161+colbyj427@users.noreply.github.com>
2025-07-08 15:19:23 -0600

    Delete build directory

--- build/3rd/lemon-1.3.1/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/colbyjohnson/Desktop/ScaleUntrim/3rd/lemon-1.3.1
-# Build directory: /Users/colbyjohnson/Desktop/ScaleUntrim/build/3rd/lemon-1.3.1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lemon")
