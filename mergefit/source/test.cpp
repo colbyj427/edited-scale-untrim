3bea3d51c8bfbadfbaa78ec760e393bc86997132
Colby Johnson <colbyj427>
2025-07-15 10:55:01 -0600

    Removed comments from test.cpp

--- mergefit/source/test.cpp
+++ mergefit/source/test.cpp
@@ -3,17 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-    // My addition
     if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " input.obj output.obj [config.txt]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " input.obj [setting.config]" << std::endl;
         return 1;
     }
     std::string cad_in = argv[1];
-    std::string cad_out = argv[2];
-    std::string config_file = (argc > 3) ? argv[3] : "";
-    // *****
+    std::string cad_out = "";
+    std::string config_file = argv[2];
 
-    // Uncomment for original
     // std::string cad_in, cad_out, config_file;
 
     MergeFit mf;
