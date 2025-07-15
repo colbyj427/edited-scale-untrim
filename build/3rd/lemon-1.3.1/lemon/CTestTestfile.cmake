347d79c8e15a841eeddabeea8deed19a05d038d1
Colby Johnson <colbyj427>
2025-07-15 10:54:18 -0600

    Remove build directory from repo and ignore it

--- build/3rd/lemon-1.3.1/lemon/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/colbyjohnson/Desktop/ScaleUntrim/3rd/lemon-1.3.1/lemon
-# Build directory: /Users/colbyjohnson/Desktop/ScaleUntrim/build/3rd/lemon-1.3.1/lemon
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
